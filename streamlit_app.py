6031ffa8ff17577032e72e6283f0f69aec6d1c47
“priya.anil” <priya.anil@obviohealth.com>
2023-08-22 20:18:32 +0800

    change filename

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,70 +6,63 @@ from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import MinMaxScaler
 
 # Set display options to show all columns and rows without truncation
 pd.set_option('display.max_columns', None)  # Show all columns
 pd.set_option('display.max_rows', None)     # Show all rows
 
-loaded_model = joblib.load('model.pkl')
 
-def data_preprocessing(data):
-    features = data.drop(columns=['isFraud'])
-    # List of columns to drop
-    columns_to_drop = ['nameOrig', 'nameDest', 'isFlaggedFraud']
 
-    # List of categorical columns to one-hot encode
-    categorical_columns = ['amount_cluster', 'type']
-
-    # List of numerical columns to normalize
-    numerical_columns = ['step', 'amount', 'oldbalanceOrg', 'newbalanceOrig', 'oldbalanceDest', 'newbalanceDest','transaction_change', 'dest_balance_change', 'day_of_week','hour_of_day']
-
-    # Custom transformer for feature engineering
-    class FeatureEngineeringTransformer(BaseEstimator, TransformerMixin):
+class ColumnDropper(BaseEstimator,TransformerMixin):
         def __init__(self):
             pass
 
-        def fit(self, X, y=None):
+        def fit(self, X, Y = None):
             return self
 
-        def transform(self, X):
-            data = X.copy()
+        def transform(self, X, Y = None):
+            X = X.drop(['isFlaggedFraud'], axis = 1)
+            return X
+def newDesAmountAfterTransactionColumn(inputDf):
+            destAmountChange = inputDf['newbalanceDest'] - inputDf['oldbalanceDest']
+            inputDf['DestAmountAfterTransaction'] = abs(destAmountChange)
+            return inputDf
+def applyCondition(inputDf):
+            inputDf = inputDf[(inputDf['amount'] != 0) & (inputDf['DestAmountAfterTransaction'] != 0)]
+            return inputDf
 
-            # Calculate Transaction Change
-            data['transaction_change'] = data['newbalanceOrig'] - data['oldbalanceOrg']
+def transformOneHot(inputDf, selected_payment_type):
+            type_selected = selected_payment_type
+            # dummies = pd.get_dummies(inputDf['type'])
+            type_list = {"CASH_IN":'', "CASH_OUT":'', "DEBIT":'', "PAYMENT":'', "TRANSFER":''}
+            my_dict = type_list.fromkeys(type_list, 0)
+            # print(my_dict)
+            if type_selected in my_dict.keys():
+                my_dict.update({type_selected: 1})
+            my_dict = {k:[v] for k,v in my_dict.items()} 
+            my_dict = pd.DataFrame.from_dict(my_dict)
+            # print(my_dict)
+            inputDf  = pd.concat([inputDf , my_dict], axis = 1)
+            inputDf = inputDf.drop(['type', 'nameOrig', 'nameDest', 'isFraud', "PAYMENT"], axis = 1)
+            # print(inputDf.head())
+            return inputDf       
 
-            # Calculate Destination Balance Change
-            data['dest_balance_change'] = data['newbalanceDest'] - data['oldbalanceDest']
-
-            # Extract Day of the Week and Hour of the Day
-            data['day_of_week'] = data['step'] % 7
-            data['hour_of_day'] = data['step'] % 24
-
-            # Create Transaction Amount Clusters
-            amount_bins = [0, 100, 500, 1000, np.inf]
-            amount_labels = ['small', 'medium', 'large', 'extra_large']
-            data['amount_cluster'] = pd.cut(data['amount'], bins=amount_bins, labels=amount_labels)
-            return data
-
-    # Create a ColumnTransformer for one-hot encoding and normalization
-    preprocessor = ColumnTransformer(
-        transformers=[
-            ('drop', 'drop', columns_to_drop),
-            ('cat', OneHotEncoder(), categorical_columns),
-            ('num', StandardScaler(), numerical_columns)
-        ],
-        remainder='passthrough'  # Keep non-categorical, non-numerical columns unchanged
-    )
-
-    # Create the data preprocessing pipeline
-    data_pipeline = Pipeline(steps=[
-        ('feature_engineering', FeatureEngineeringTransformer()),
-        ('preprocessor', preprocessor)
-    ])
-
-    # Apply the pipeline to your data
-    data_preprocessed = data_pipeline.fit_transform(features)
-    return data_preprocessed
+def preprocessing(inputDf, selected_payment_type):
+            df_1 = newDesAmountAfterTransactionColumn(inputDf)
+            df_2 = applyCondition(df_1)
+            df_2 = transformOneHot(df_2, selected_payment_type)
+            
+            # main_pipeline = Pipeline([
+            # ('dropColumns',ColumnDropper() ),
+            # ("MinMaxScaler", MinMaxScaler()),
+            # ('RandomClassifier',RandomForestClassifier(n_estimators = 500, max_depth=7, random_state=42) )
+    
+            # ])
+            print(df_2)
+            return df_2
+    
 
 def main():
     st.title("Fraud Detection App")
@@ -90,25 +83,28 @@ def main():
         #Prepare the input data as a DataFrame.
         #Note: Only the value in the index 0 of the data array is actual input. The rest are added to make sure the one hot encoder is working the same as the training pipeline. 
         input_data = pd.DataFrame({
-            "step": [steps, steps, steps,steps,steps,steps,steps],
-            "type": [selected_payment_type, "CASH_IN", "CASH_OUT", "DEBIT", "PAYMENT", "TRANSFER", "TRANSFER"],
-            "amount": [amount, 1, 101, 501, 1001, 10001, 0],
-            "nameOrig": ["Dummy Value", "Dummy Value","Dummy Value","Dummy Value","Dummy Value","Dummy Value","Dummy Value"],
-            "oldbalanceOrg": [oldbalanceOrg, 0, 0, 0, 0, 0, 0],
-            "newbalanceOrig": [newbalanceOrig, 0, 0, 0, 0, 0, 0],
-            "nameDest": ["Dummy Value","Dummy Value","Dummy Value","Dummy Value","Dummy Value","Dummy Value","Dummy Value"],
-            "oldbalanceDest": [oldbalanceDest, 0, 0, 0, 0, 0,0],
-            "newbalanceDest": [newbalanceDest, 0, 0, 0, 0, 0,0],
-            "isFraud": [0, 0, 0, 0, 0, 0,0],
-            "isFlaggedFraud": [0, 0, 0, 0, 0, 0,0]
+            "step": [steps],
+            "type": [selected_payment_type],
+            "amount": [amount],
+            "nameOrig": ["Dummy Value"],
+            "oldbalanceOrg": [oldbalanceOrg],
+            "newbalanceOrig": [newbalanceOrig],
+            "nameDest": ["Dummy Value"],
+            "oldbalanceDest": [oldbalanceDest],
+            "newbalanceDest": [newbalanceDest],
+            "isFraud": [0],
+            "isFlaggedFraud": [0],
         })
-        
-        input_data = pd.DataFrame(data_preprocessing(input_data))
-
-        # Make a prediction using the loaded model
-        prediction = loaded_model.predict(input_data)
-        
-        print(prediction)
+        # print('before',input)
+        # input_data_df = pd.DataFrame(input_data)
+        processed_input_df = preprocessing(input_data, selected_payment_type)
+        print('processed data',processed_input_df)
+        print(input_data)
+        loaded_model = joblib.load('model_ahshik.pkl')
+        # # Make a prediction using the loaded model
+        prediction = loaded_model.predict(processed_input_df)
+        # print('after',input_data)
+        # print(prediction)
         
         # Display the prediction result
         if prediction[0] == 1:
